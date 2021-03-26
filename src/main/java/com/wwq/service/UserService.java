9498f53c37bfca4f73d8b3cefab21bf9ca92460b
WangWeiQing <775242373@qq.com>
2021-03-26 19:55:08 +0800

    UserService及impl查询方法增加

--- src/main/java/com/wwq/service/UserService.java
+++ src/main/java/com/wwq/service/UserService.java
@@ -1,4 +1,9 @@
 package com.wwq.service;
 
+import com.wwq.bean.User;
+
+import java.util.List;
+
 public interface UserService {
+    List<User> queryByName(User user);
 }
--- src/main/java/com/wwq/service/impl/UserServiceImpl.java
+++ src/main/java/com/wwq/service/impl/UserServiceImpl.java
@@ -1,11 +1,27 @@
 package com.wwq.service.impl;
 
+import com.wwq.bean.User;
+import com.wwq.dao.UserDao;
+import com.wwq.dao.impl.UserDaoImpl;
 import com.wwq.service.UserService;
+
+import java.util.List;
 
 public class UserServiceImpl implements UserService {
     private static UserService userService = new UserServiceImpl();
     private UserServiceImpl(){}
     public static UserService getInstance(){
         return userService;
+    }
+
+    //获取userDao对象
+    UserDao userDao = UserDaoImpl.getInstance();
+
+    @Override
+    public List<User> queryByName(User user) {
+        if (user == null) {
+            return null;
+        }
+        return userDao.queryByName(user);
     }
 }
