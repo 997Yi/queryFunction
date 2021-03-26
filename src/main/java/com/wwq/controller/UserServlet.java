1558fb08a417a0662dc1afe118a76c48905c7233
WangWeiQing <775242373@qq.com>
2021-03-26 20:31:46 +0800

    index.jsp的输出编写

--- src/main/java/com/wwq/controller/UserServlet.java
+++ src/main/java/com/wwq/controller/UserServlet.java
@@ -1,19 +1,30 @@
 package com.wwq.controller;
 
+import com.wwq.bean.User;
+import com.wwq.service.UserService;
+import com.wwq.service.impl.UserServiceImpl;
+
 import javax.servlet.*;
 import javax.servlet.http.*;
 import java.io.IOException;
+import java.util.List;
 import javax.servlet.annotation.WebServlet;
 
 @WebServlet("/index")
 public class UserServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.getRequestDispatcher("/index.jsp").forward(request,response);
+        doPost(request, response);
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        doGet(request, response);
+        UserService userService = UserServiceImpl.getInstance();
+        List<User> userList = userService.queryByName(new User());
+        if (!userList.isEmpty()){
+            System.out.println(userList);
+            request.setAttribute("userList", userList);
+        }
+        request.getRequestDispatcher("/index.jsp").forward(request,response);
     }
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -6,11 +6,20 @@
   To change this template use File | Settings | File Templates.
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <html>
 <head>
     <title>Title</title>
 </head>
 <body>
-<h1>Hello World!</h1>
+<c:if test="${userList != null}">
+    <h2>用户列表</h2>
+    <c:forEach items="${userList}" var="user">
+        ${user.name} ${user.gender} ${user.birthday}
+    </c:forEach>
+</c:if>
+<c:if test="${userList == null}">
+    <h2>未查询到指定用户</h2>
+</c:if>
 </body>
 </html>
