d88c7032d9f28d0733cc41fc7ad21db2e4cdc020
WangWeiQing <775242373@qq.com>
2021-03-26 21:20:11 +0800

    form表单编辑，通过表单提交数据后台封装查询编写，编码集过滤器编写

--- src/main/java/com/wwq/controller/UserServlet.java
+++ src/main/java/com/wwq/controller/UserServlet.java
@@ -7,6 +7,9 @@ import com.wwq.service.impl.UserServiceImpl;
 import javax.servlet.*;
 import javax.servlet.http.*;
 import java.io.IOException;
+import java.sql.Date;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.List;
 import javax.servlet.annotation.WebServlet;
 
@@ -20,8 +23,22 @@ public class UserServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         UserService userService = UserServiceImpl.getInstance();
-        List<User> userList = userService.queryByName(new User());
-        if (!userList.isEmpty()){
+        //封装数据
+        User user = new User();
+        user.setName((String)request.getParameter("name"));
+        user.setGender((String)request.getParameter("gender"));
+        String strDate = (String)request.getParameter("birthday");
+        Date date = null;
+        try {
+            date = new Date(new SimpleDateFormat("yyyy-MM-dd").parse(strDate).getTime());
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        user.setBirthday(date);
+
+        //查询数据
+        List<User> userList = userService.queryByName(user);
+        if (!userList.isEmpty() || userList == null){
             System.out.println(userList);
             request.setAttribute("userList", userList);
         }
---
+++ src/main/webapp/form.jsp
@@ -0,0 +1,23 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: 77524
+  Date: 2021/3/26
+  Time: 21:00
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+
+<form action="index" method="post">
+    姓名：<input type="text" name="name"/><br>
+    性别：<input type="text" name="gender"/><br>
+    生日：<input type="text" name="birthday"/><br>
+    <input type="submit" value="查询">
+    <input type="reset" value="重置">
+</form>
+</body>
+</html>
